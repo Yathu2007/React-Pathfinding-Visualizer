5ba8b23496ddadd4b73244afb7e901da7e855860
Yathurshan Manoharan <prochessgamer01@gmail.com>
2022-07-29 22:36:05 +0400

    styled the sidebar with some discord flavour

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,15 +1,54 @@
+import React, { useState } from "react";
+
+import { BsFillLightningChargeFill, BsCpuFill } from "react-icons/bs";
 import { TbBarrierBlock } from "react-icons/tb";
+import { FaPlay, FaCog, FaSun, FaMoon } from "react-icons/fa";
 
 const SideBar = () => {
+    const [darkMode, setTheme] = useState(true);
+
     return (
-        <div className="fixed top-0 h-screen w-16 flex flex-col text-white  bg-slate-400 align-top shadow-lg">
-            <SideBarIcon icon={<TbBarrierBlock />} />
+        // top part
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col text-white m-0  bg-primary drop-shadow-xl items-center">
+            <div className="flex justify-start">
+                <SideBarIcon
+                    icon={<BsFillLightningChargeFill size={25} />}
+                    tooltip="Pathfinding visualizer"
+                />
+            </div>
+
+            <div className="separator"></div>
+
+            <SideBarIcon icon={<FaPlay size={20} />} tooltip="play animation" />
+
+            <SideBarIcon icon={<BsCpuFill size={25} />} tooltip="algorithm" />
+
+            <SideBarIcon
+                icon={<TbBarrierBlock size={25} />}
+                tooltip="add barrier"
+            />
+
+            <div className="separator"></div>
+
+            <SideBarIcon
+                icon={!darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+                tooltip="theme"
+                click={() => setTheme(!darkMode)}
+            />
+            <SideBarIcon icon={<FaCog size={20} />} tooltip="settings" />
         </div>
     );
 };
 
-const SideBarIcon = ({ icon }) => {
-    return <button className="sidebar-icon">{icon}</button>;
+const SideBarIcon = ({ icon, tooltip, click }) => {
+    return (
+        <button className="sidebar-icon group" onClick={() => click()}>
+            {icon}
+            <span className="sidebar-tooltip group-hover:scale-100 transition-all group-active:scale-0">
+                {tooltip}
+            </span>
+        </button>
+    );
 };
 
 export default SideBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#202225",
+                secondary: "#5865f2",
+
+                gray: {
+                    900: "#202225",
+                    800: "#2F3136",
+                    700: "#36393F",
+                    600: "#4F545C",
+                },
+            },
+        },
     },
     plugins: [],
 };
